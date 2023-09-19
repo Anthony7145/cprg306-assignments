e3bcd69f526fe7416126340e60f99d6c7911c5b8
Anthony7145 <119919902+Anthony7145@users.noreply.github.com>
2023-09-19 13:36:39 -0600

    finished assignment 2

---
+++ app/StudentInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h1>Anthony</h1>
+            <p>CPRG 306 F</p>
+            <a href="https://github.com/Anthony7145">GitHub Repository</a>
+        </div>
+    );
+}
+
---
+++ app/week2/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+import StudentInfo from '../StudentInfo';
+
+export default function Week2Page() {
+  return (
+    <main>
+      <h1>My Shopping List</h1>
+      <StudentInfo />
+    </main>
+  );
+}
+
