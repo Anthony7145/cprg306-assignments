b74c6ed110eed48b909f7a3d2ba0b56f90638737
Idemili Ifechukwu Anthony <119919902+Anthony7145@users.noreply.github.com>
2023-10-18 09:17:53 -0600

    Delete week3 directory

--- week3/header.js
+++
@@ -1,3 +0,0 @@
-export default function Header({title}) {
-  return <h2 className="text-2xl font-bold m-2 text-yellow-400">{title}</h2>;
-}
--- week3/page.js
+++
@@ -1,9 +0,0 @@
-import ItemList from "./item-list";
-
-export default function Page() {
-  return (
-    <main className="big-grey-100 p-4">
-      <ItemList />
-    </main>
-  );
-}
