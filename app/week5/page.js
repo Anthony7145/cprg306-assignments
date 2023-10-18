de7501ce38a401098a5346d0e756f8a269d297fb
Idemili Ifechukwu Anthony <119919902+Anthony7145@users.noreply.github.com>
2023-10-18 09:17:21 -0600

    Add files via upload

---
+++ app/week5/page.js
@@ -0,0 +1,12 @@
+import ItemList from "./item-list";
+
+
+export default function Page() {
+  return (
+    <main className="bg-[#bacee2] h-screen bg-gray w-full  justify-center  p-8 " >
+      <h1 className="text-4xl font-bold m-6 text-center text-yellow-300">  
+      </h1>
+      <ItemList />
+    </main>
+  );
+} 
